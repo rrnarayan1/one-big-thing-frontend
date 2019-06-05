30569aeddefe915d7ac56c8ea308c601d2ad11d5
Rohan <rrnarayan1@gmail.com>
2019-06-05 11:01:45 -0700

    added other team link

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import queryString from 'query-string';
-//import axios from 'axios';
+import axios from 'axios';
 //import { Row, Col } from 'react-bootstrap';
 import ScoreItem from './ScoreItem.js';
 import ScoreItemGrid from './ScoreItemGrid.js';
@@ -18,32 +18,32 @@ class Game extends React.Component {
   }
 
   componentDidMount() {
-    // axios.get('http://35.203.94.32:5000/score', {
-    //   params: {
-    //     seasonId : this.params.seasonId,
-    //     teamId : this.params.teamId,
-    //     gameId : this.params.gameId
-    //   }
-    // })
-    //   .then(response => {
-    //      const data = response.data;
-    //      console.log(data);
-    //      this.setState(data);
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //   })
-    const data = {"stats":{"AST":21.0,"BLK":1.0,"DREB":33.0,"FG3A":27.0,"FG3M":9.0,"FG3_PCT":0.333,"FGA":77.0,"FGM":34.0,"FG_PCT":0.442,"FTA":31.0,"FTM":19.0,"FT_PCT":0.613,"STL":12.0, "TOV":15.0, "OREB":11.0, "PTS":96.0,"REB":44.0}, "data":"22016","game":{"AST":21.0,"BLK":1.0,"DREB":33.0,"FG3A":27.0,"FG3M":9.0,"FG3_PCT":0.333,"FGA":77.0,"FGM":34.0,"FG_PCT":0.442,"FTA":31.0,"FTM":19.0,"FT_PCT":0.613,"GAME_DATE":"Sun, 05 Mar 2017 00:00:00 GMT","GAME_ID":"0021600927","GAME_NUM":62,"MATCHUP":"ATL vs. IND","OREB":11.0,"PF":19.0,"PLUS_MINUS":-1.0,"PTS":96.0,"REB":44.0,"SEASON_ID":"22016","STL":12.0,"TEAM_ABBREVIATION":"ATL","TEAM_ID":"1610612737","TEAM_NAME":"Atlanta Hawks","TOV":15.0,"WL":"L"},"obt":{"absolute":{"score":-1.65,"stat":"BLK"},"negative":{"score":-1.65,"stat":"BLK"},"positive":{"score":1.17,"stat":"STL"}},"portion":"before","scores":{"AST":-0.55,"BLK":-1.65,"DREB":-0.18,"FG3A":0.25,"FG3M":0.07,"FG3_PCT":-0.11,"FGA":-1.01,"FGM":-0.83,"FG_PCT":-0.26,"FTA":0.78,"FTM":0.2,"FT_PCT":-1.07,"OREB":0.25,"PTS":-0.61,"REB":-0.01,"STL":1.17,"TOV":-0.1}}
+    axios.get('http://127.0.0.1:5000/score', {
+      params: {
+        seasonId : this.params.seasonId,
+        teamId : this.params.teamId,
+        gameId : this.params.gameId
+      }
+    })
+      .then(response => {
+         const data = response.data;
+         this.setState({data: data});
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    // const data = {"stats":{"AST":21.0,"BLK":1.0,"DREB":33.0,"FG3A":27.0,"FG3M":9.0,"FG3_PCT":0.333,"FGA":77.0,"FGM":34.0,"FG_PCT":0.442,"FTA":31.0,"FTM":19.0,"FT_PCT":0.613,"STL":12.0, "TOV":15.0, "OREB":11.0, "PTS":96.0,"REB":44.0}, "data":"22016","game":{"AST":21.0,"BLK":1.0,"DREB":33.0,"FG3A":27.0,"FG3M":9.0,"FG3_PCT":0.333,"FGA":77.0,"FGM":34.0,"FG_PCT":0.442,"FTA":31.0,"FTM":19.0,"FT_PCT":0.613,"GAME_DATE":"Sun, 05 Mar 2017 00:00:00 GMT","GAME_ID":"0021600927","GAME_NUM":62,"MATCHUP":"ATL vs. IND","OREB":11.0,"PF":19.0,"PLUS_MINUS":-1.0,"PTS":96.0,"REB":44.0,"SEASON_ID":"22016","STL":12.0,"TEAM_ABBREVIATION":"ATL","TEAM_ID":"1610612737","TEAM_NAME":"Atlanta Hawks","TOV":15.0,"WL":"L"},"obt":{"absolute":{"score":-1.65,"stat":"BLK"},"negative":{"score":-1.65,"stat":"BLK"},"positive":{"score":1.17,"stat":"STL"}},"portion":"before","scores":{"AST":-0.55,"BLK":-1.65,"DREB":-0.18,"FG3A":0.25,"FG3M":0.07,"FG3_PCT":-0.11,"FGA":-1.01,"FGM":-0.83,"FG_PCT":-0.26,"FTA":0.78,"FTM":0.2,"FT_PCT":-1.07,"OREB":0.25,"PTS":-0.61,"REB":-0.01,"STL":1.17,"TOV":-0.1}}
 
-    this.setState(data);
+    // this.setState(data);
   }
 
   render() {
-    const game = this.state.game;
-    if (game) {
-      console.log(this.state)
-      const obt = this.state.obt;
-      const pts = game["PTS"];
+    const data = this.state.data;
+    if (data.game) {
+      console.log(data)
+      const game = data.game
+      const obt = data.obt;
+      const pts = data.stats["PTS"];
       const opp_pts = pts + game["PLUS_MINUS"];
       const score = String(pts)+"-"+String(opp_pts);
       const date = new Date(Date.parse(game["GAME_DATE"]));
@@ -52,7 +52,11 @@ class Game extends React.Component {
         <div className="Game">
           <div className="Game--header">
             <div className="Game--header-left">
-              <h2>{game["TEAM_NAME"]}</h2>
+              <h2>{data.team["TEAM_NAME"]} 
+                <span id="Game--header-opp-team"> vs <a id="Game--header-opp-team" href={"/game?seasonId="+this.params.seasonId+"&teamId="+data.opp_team["TEAM_ID"]+"&gameId="+this.params.gameId}>
+                  {data.opp_team["TEAM_NAME"]}</a>
+                 </span>
+               </h2>
               <div className="Game--stats">
                 {date_str + " | "}
                 {game["WL"]+" | "}
@@ -68,18 +72,18 @@ class Game extends React.Component {
             </div>
           </div>
           <div className="Game--content">
+            <ScoreItemGrid
+              scores={data.scores}
+              stats={data.stats}
+              showStatDefault={this.state.showStats}>
+            </ScoreItemGrid>
             <Button
+              className="Game--stats-button"
               variant="outline-secondary"
               onClick={() => this.setState({showStats: !this.state.showStats})}>
               Show All {this.state.showStats ? "Scores" : "Stats"}
             </Button>
-            <ScoreItemGrid
-              scores={this.state.scores}
-              stats={this.state.stats}
-              showStatDefault={this.state.showStats}>
-            </ScoreItemGrid>
           </div>
-          <p> Hover to see {this.state.showStats ? "scores" : "stats"} </p>
         </div>
 
       );
